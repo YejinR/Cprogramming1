8041eccfdb8f090f5248ecc9c025f9e2260f0f73
YejinR <yyj3647@gmail.com>
2024-09-18 13:58:23 +0900

    hw1 업로드

---
+++ hw1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int num1, num2;
+	printf("2개의 정수를 입력하세요: ");
+	scanf("%d%d", &num1, &num2);
+	int result1 = num1&num2;
+	int result2 = num1|num2;
+	int result3 = num1^num2;
+	printf("%d & %d = %d\n", num1, num2, result1);
+	printf("%d | %d = %d\n", num1, num2, result2);
+	printf("%d ^ %d = %d\n", num1, num2, result3);
+	return 0;
+}
