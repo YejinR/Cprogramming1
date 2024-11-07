1eb1b2f0a6adcdcbc1b755eb77e6ff6f2a5472ea
YejinR <yyj3647@gmail.com>
2024-11-07 16:45:04 +0900

    hw7 업로드

---
+++ hw7.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int arr1[6] = { 1, 2, 3, 4, 5, 6 };
+	int arr2[6] = { 7, 8, 9, 10, 11, 12 };
+	int* ptr1 = arr1, * ptr2 = arr2;
+	int* temp;
+
+	printf("arr1: ");
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", arr1[i]);
+	}
+
+	printf("\narr2: ");
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", arr2[i]);
+	}
+
+	for (int i = 0; i < 6; i++) {
+		temp = *(ptr1 + i);
+		*(ptr1 + i) = *(ptr2 + i);
+		*(ptr2 + i) = temp;
+	}
+	printf("\n");
+
+	printf("\nafter swap");
+	printf("\narr1: ");
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", arr1[i]);
+	}
+	printf("\narr2: ");
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", arr2[i]);
+	}
+
+
+
+	return 0;
+}
